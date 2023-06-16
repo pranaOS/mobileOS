b5562a6bc44ebda4c3616b05c839ca1838ec9422
krishpranav <krisna.pranav@gmail.com>
2023-06-16 22:22:02 +0530

    libraries: libutils[integer]

--- libraries/libutils/integer.hpp
+++ libraries/libutils/integer.hpp
@@ -14,22 +14,50 @@
 #include <cstdint>
 
 namespace Utils::integer {
+
+    /**
+     * @brief BitsInByte
+     * 
+     */
     constexpr inline auto BitsInByte = 8U;
 
-    template <typename T> 
+    /**
+     * @brief TypeHolder
+     * 
+     * @tparam T 
+     */
+    template <typename T>
     struct TypeHolder {
         using type = T;
     };
 
-    template<unsigned int Bits>
+
+    /**
+     * @brief Get the Integer Type object
+     * 
+     * @tparam Bits 
+     * @return auto 
+     */
+    template <unsigned int Bits>
     auto getIntegerType() {
         static_assert(Bits <= 64);
 
-        if constexpr(Bits <= 8) {
+        /**
+         * @brief TypeHolder
+         * 
+         */
+        if constexpr (Bits <= 8) {
             return TypeHolder<std::uint8_t>();
         }
         else if constexpr (Bits <= 16) {
             return TypeHolder<std::uint16_t>();
         }
+        else if constexpr (Bits <= 32) {
+            return TypeHolder<std::uint32_t>();
+        }
+        else {
+            return TypeHolder<std::uint64_t>();
+        }
     }
-}
+
+} 
