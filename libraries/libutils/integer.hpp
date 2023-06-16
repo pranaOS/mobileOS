81c434c08bc43a3dc4d26906e5e7854e1cfc778b
krishpranav <krisna.pranav@gmail.com>
2023-06-16 22:21:13 +0530

    libraries: libutils[integer]

--- libraries/libutils/integer.hpp
+++ libraries/libutils/integer.hpp
@@ -18,6 +18,18 @@ namespace Utils::integer {
 
     template <typename T> 
     struct TypeHolder {
-        
+        using type = T;
+    };
+
+    template<unsigned int Bits>
+    auto getIntegerType() {
+        static_assert(Bits <= 64);
+
+        if constexpr(Bits <= 8) {
+            return TypeHolder<std::uint8_t>();
+        }
+        else if constexpr (Bits <= 16) {
+            return TypeHolder<std::uint16_t>();
+        }
     }
 }
