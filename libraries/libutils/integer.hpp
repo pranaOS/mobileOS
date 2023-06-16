fc7f9aa77ed11fe6bc58c8404fe47952da0559d7
krishpranav <krisna.pranav@gmail.com>
2023-06-16 22:19:55 +0530

    libraries: libutils[integer]

---
+++ libraries/libutils/integer.hpp
@@ -0,0 +1,23 @@
+/**
+ * @file integer.hpp
+ * @author Krisna Pranav
+ * @brief Integer
+ * @version 0.1
+ * @date 2023-06-16
+ * 
+ * @copyright Copyright (c) 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <cstdint>
+
+namespace Utils::integer {
+    constexpr inline auto BitsInByte = 8U;
+
+    template <typename T> 
+    struct TypeHolder {
+        
+    }
+}
